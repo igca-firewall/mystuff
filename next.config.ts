0a3e3983dc7ad130b0ffed05753a23ac20233c25
Ebenezer-1000 <ebenezerchinomso9@gmail.com>
2024-12-03 00:33:30 +1300

    second commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript:{
+    ignoreBuildErrors: true,
+
+  },
   /* config options here */
+  images:{
+    domains: [
+      'cloud.appwrite.io',
+      'platform-lookaside.fbsbx.com',
+      'lh3.googleusercontent.com',
+      // {
+      //   domain: 'your-domain.com',
+      //   public: '/_next/static/images',
+      // },
+    ],
+  }
 };
 
 export default nextConfig;
