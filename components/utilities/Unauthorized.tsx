dad8277184b723b2ee40aa1e06c7fa2d0ee9d6a3
Ebenezer-1000 <ebenezerchinomso9@gmail.com>
2024-12-16 07:24:16 +1300

    Xed 5

--- components/utilities/Unauthorized.tsx
+++ components/utilities/Unauthorized.tsx
@@ -12,12 +12,7 @@ const Unauthorized = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b  text-white px-6">
       {/* SEO Optimization */}
-      <head>
-        <title>Unauthorized Access</title>
-        <meta name="description" content="You do not have access to this page. Please contact the administrator for assistance." />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </head>
-
+    
       {/* Lock Icon */}
       <div className="bg-gray-700 p-6 rounded-full shadow-lg mb-8">
         <FaLock className="text-gray-300 text-5xl" />
@@ -41,7 +36,7 @@ const Unauthorized = () => {
         </button>
 
         <button
-          onClick={() => router.push('/contact')}
+          onClick={() => router.push('/docs')}
           className="flex items-center px-6 py-3 bg-red-600 text-white font-medium rounded-lg shadow-md hover:bg-red-500 transition focus:outline-none focus:ring focus:ring-red-500"
         >
           Contact Support
